2e7f9898d9a9cb598a20929509a5e7481e7a4839
집 <@DESKTOP-L00N2LG>
2021-01-14 11:51:22 +0900

    설정

--- container/src/main/java/container/soundsystem/CDPlayer.java
+++ container/src/main/java/container/soundsystem/CDPlayer.java
@@ -1,13 +1,40 @@
 package container.soundsystem;
 
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component    //스캐닝할것 자동으로!!
 public class CDPlayer {
 
+//  Wiring01 : 필드(reflection) 주입(CGLib)
+//	@Inject     //(자바 표준이지만 Wiring Annotation) but 잘 안씀!
 	@Autowired
-	private CompactDisc cd;
+	@Qualifier("highSchoolRapper3Final")      //자바랑 스프링것이 있는데 스프링이면 스프링껄 써야함!!
+	private CompactDisc cd;			//이름 지정해줌!!
+	
+	public CDPlayer() {
+	}
+	
+//  Wiring02 : 생성자 주입!
+//	@Autowired
+	public CDPlayer(@Qualifier("highSchoolRapper3Final") CompactDisc cd) {
+		this.cd = cd;
+	}
+	
+//  Wiring03 : setter 주입!
+//	@Autowired
+	public void setCD(@Qualifier("highSchoolRapper3Final") CompactDisc cd) {
+		this.cd = cd;
+	}
+
+//  Wiring04 : 일반 메소드 주입!	
+//	@Autowired
+	public void insertCD(@Qualifier("highSchoolRapper3Final") CompactDisc cd) {
+		this.cd = cd;
+	}
 	
 	public void play() {
 		cd.play();
