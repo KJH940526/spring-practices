a37f23f5545f3ec8ecf3475dc190d3858269eed3
집 <@DESKTOP-L00N2LG>
2021-01-14 11:02:30 +0900

    개행

--- container/src/test/java/container/soundsystem/CDPlyaerXMLConfigTest.java
+++ container/src/test/java/container/soundsystem/CDPlyaerXMLConfigTest.java
@@ -29,6 +29,7 @@ public class CDPlyaerXMLConfigTest {
 	@Test
 	public void testPlay() {
 		cdPlayer.play();
+																//프린트 ln에는 개행이 있어서 리플레이스로 바꿔줌!
 		assertEquals("playing 붕붕 by 김하온", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
 	}
 
